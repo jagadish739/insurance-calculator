213fef93ee1a17b03c41b05b59f2d26c2b3902c9
Jakub Chrzastek <j.chrzastek@gmail.com>
2017-03-05 20:34:43 +0100

    in progress slider

--- app/public/scripts/all.js
+++ app/public/scripts/all.js
@@ -3,36 +3,26 @@ var myapp = angular.module('myApp', ['rzModule', 'ui.bootstrap']);
 'use strict';
 (function() {
     angular.module('myApp')
-        .controller('calculatorController', calculatorController);
-
-    calculatorController.$inject = ['$scope'];
-
-    function calculatorController($scope) {
-        $scope.slider = {
-            value: 150,
-            options: {
-                floor: 0,
-                ceil: 450
+        .directive('calculator', [function() {
+            return {
+                restrict: 'E',
+                replace: true,
+                templateUrl: 'templates/html/_calculator.html',
+                scope: {
+                    content: '='
+                },
+                link: function($scope, $element, $attrs) {
+                    $scope.element = $element;
+                },
+                controller: function($scope) {
+                    $scope.slider = {
+                        value: 50000,
+                        options: {
+                            floor: 100,
+                            ceil: 100000
+                        }
+                    };
+                }
             }
-        };
-    }
-})();
-'use strict';
-(function() {
-angular.module('myApp')
-    .directive('calculator', [function() {
-        return {
-            restrict: 'E',
-            replace: true,
-            templateUrl: 'templates/html/_calculator.html',
-            scope: {
-                content: '='
-            },
-            link: function($scope, $element, $attrs) {
-                $scope.element = $element;
-            },
-            controller: function($scope) {
-            }
-        }
-    }]);
+        }]);
 })();
--- app/scripts/directives/calculator.js
+++ app/scripts/directives/calculator.js
@@ -1,19 +1,26 @@
 'use strict';
 (function() {
-angular.module('myApp')
-    .directive('calculator', [function() {
-        return {
-            restrict: 'E',
-            replace: true,
-            templateUrl: 'templates/html/_calculator.html',
-            scope: {
-                content: '='
-            },
-            link: function($scope, $element, $attrs) {
-                $scope.element = $element;
-            },
-            controller: function($scope) {
+    angular.module('myApp')
+        .directive('calculator', [function() {
+            return {
+                restrict: 'E',
+                replace: true,
+                templateUrl: 'templates/html/_calculator.html',
+                scope: {
+                    content: '='
+                },
+                link: function($scope, $element, $attrs) {
+                    $scope.element = $element;
+                },
+                controller: function($scope) {
+                    $scope.slider = {
+                        value: 50000,
+                        options: {
+                            floor: 100,
+                            ceil: 100000
+                        }
+                    };
+                }
             }
-        }
-    }]);
+        }]);
 })();
