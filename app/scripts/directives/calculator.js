d2e08a1d0515f11de2fe248793a513ccf99ae5fe
Jakub Chrzastek <j.chrzastek@gmail.com>
2017-03-06 01:44:36 +0100

    es6 format, add installment calculator for button

--- app/scripts/directives/calculator.js
+++ app/scripts/directives/calculator.js
@@ -13,14 +13,56 @@
                     $scope.element = $element;
                 },
                 controller: function($scope) {
-                    $scope.slider = {
-                        value: 50000,
-                        options: {
-                            floor: 100,
-                            ceil: 100000
+                        $scope.slider = {
+                            value: 5000,
+                            options: {
+                                floor: 100,
+                                ceil: 10000
+                            }
+                        };
+
+                        const Value = [{
+                            min: 100,
+                            max: 1000,
+                            value: 20
+                        }, {
+                            min: 1001,
+                            max: 3000,
+                            value: 70
+                        }, {
+                            min: 3001,
+                            max: 6000,
+                            value: 130
+                        }, {
+                            min: 6001,
+                            max: 9000,
+                            value: 180
+                        }, {
+                            min: 9001,
+                            max: 10000,
+                            value: 200
+                        }];
+
+                        let value = $scope.slider.value;
+                        $scope.$watch('slider.value', function findElement(value) {
+                            const iterator = (item) =>
+                                item.min <= value && item.max >= value;
+                            let obj = Value.find(iterator);
+                            $scope.rata = obj.value;
+                        })
+
+                        let isClicked = false;
+                        $scope.installment = (rata) => {
+                            if (!isClicked) {
+                                let wynik = Math.round($scope.rata * 0.95);
+                                console.log("wynik: " + wynik);
+                                isClicked = !isClicked;
+                            } else {
+                                console.log("Przed zmiana" + $scope.rata);
+                                isClicked = !isClicked;
+                            }
                         }
-                    };
-                }
-            }
+                    } //end controller
+            } //end return
         }]);
 })();
